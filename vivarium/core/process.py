292e92f4e105b48b0719a5414cbfbb5393213025
Eran Agmon <agmon.eran@gmail.com>
2020-06-17 17:48:56 -0700

    redo process constructor

--- vivarium/core/process.py
+++ vivarium/core/process.py
@@ -1,20 +1,29 @@
 from __future__ import absolute_import, division, print_function
 
+import copy
+
+from vivarium.library.dict_utils import deep_merge
+
+DEFAULT_TIME_STEP = 1.0
+
 
 class Process(object):
-    def __init__(self, ports, parameters=None):
-        ''' Declare what ports this process expects. '''
 
-        self.ports = ports
-        self.parameters = parameters or {}
-        self.time_step = parameters.get('time_step', 1.0)
+    defaults = {}
+
+    def __init__(self, ports, parameters=None):
+        if parameters is None:
+             parameters = {}
+        self.parameters = copy.deepcopy(self.defaults)
+        deep_merge(self.parameters, parameters)
+        self.ports = ports  # list(self.ports_schema().keys())
 
     def local_timestep(self):
         '''
         Returns the favored timestep for this process.
         Meant to be overridden in subclasses, unless 1.0 is a happy value.
         '''
-        return self.time_step
+        return self.parameters.get('time_step', DEFAULT_TIME_STEP)
 
     def default_state(self):
         '''
@@ -69,7 +78,6 @@ class Process(object):
     def next_update(self, timestep, states):
         '''
         Find the next update given the current states this process cares about.
-
         This is the main function a new process would override.'''
 
         return {
